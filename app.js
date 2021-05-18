6249372f2f45f511787e66cafd7cc68ddcddfebb
fer2021 <fernandorodriguez@smsconsulting.mobi>
2021-05-18 11:38:35 -0300

    detalleproducto

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const express = require('express')
-const path = require ('path');
+const path = require('path');
 const app = express();
 
 app.use(express.static('Public'));
 
-app.listen(3050,() => console.log('Servidor corriendo en el puerto 3050'));
+app.listen(3050, () => console.log('Servidor corriendo en el puerto 3050'));
 
-app.get('/', function (req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/index.html')
+app.get('/', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/index.html')
     res.sendFile(htmlPath)
-    //console.log(__dirname);
-    //        res.send(producto);
+        //console.log(__dirname);
+        //        res.send(producto);
 });
 
-app.get('/login', function (req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/login.html')
-    res.sendFile(htmlPath)
-});
-
-app.get('/reset_account', function (req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/reset_account.html')
+app.get('/login', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/login.html')
     res.sendFile(htmlPath)
 });
 
-app.get('/register', function (req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/register.html')
+app.get('/reset_account', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/reset_account.html')
     res.sendFile(htmlPath)
 });
 
-app.get('/market', function (req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/market.html')
+app.get('/register', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/register.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/market', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/market.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/detalleProducto', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './Views/detalleProducto.html')
     res.sendFile(htmlPath)
 });
