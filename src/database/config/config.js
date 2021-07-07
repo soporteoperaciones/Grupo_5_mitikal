970aab42b4a8c62e92ce7e6d1071df47059fc54f
fer2021 <fernandorodriguez@smsconsulting.mobi>
2021-07-07 18:43:29 -0300

    secuelize + db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,9 +2,10 @@ module.exports = {
     "development": {
         "username": "root",
         "password": null,
-        "database": "database_development",
+        "database": "mitikal",
         "host": "127.0.0.1",
         "dialect": "mysql"
+        "port": 3306
     },
     "test": {
         "username": "root",
---
+++ src/database/models/User.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'ModelUser'
+    const columns = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        name: {
+            type: DataTypes.STRING
+
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        image: {
+            type: DataTypes.STRING(200),
+        },
+        tel: {
+            type: DataTypes.STRING,
+        },
+        password1: {
+            type: DataTypes.STRING,
+        },
+        password2: {
+            type: DataTypes.STRING,
+        }
+    }
+    const config = {
+        tableName: 'user',
+        timestamps: true,
+
+    }
+
+    const Model = sequelize.define(alias, columns, config);
+    return Model;
+}
