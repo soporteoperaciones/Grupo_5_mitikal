39589fafda79f6847133b051c6bc121ab047d2e2
gagomez85 <gagomez85@gmail.com>
2021-08-07 02:20:28 -0300

    Sprint 6 - index - random

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,17 @@
 const path = require('path')
+const { Product } = require('../database/models')
 
 const indexController = {
-    index: (req, res) => {
-        return res.render('index')
+    index: async(req, res) => {
+
+        const productList = await Product.findAll({
+            order: [
+                ['name', 'ASC']
+            ],
+        })
+
+        return res.render('index', { productList })
+    
     }
 
 }
